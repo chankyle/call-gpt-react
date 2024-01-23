d19ff8d817059947b7ef0a0aab348e9b079666ab
Ben Johnstone <benji.forrest@gmail.com>
2024-01-23 12:55:20 -0700

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,111 +1,24 @@
 // Import from react-router-dom
-import { Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 // Import Bootstrap
-import { Nav, Navbar, Container, Dropdown, Row, Col } from "react-bootstrap";
+import { Navbar, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 // Components
 import Home from "./components/Home";
-import VerificationForm from "./components/VerificationForm";
-import EventStream from "./components/EventStream";
-import CheckVerification from "./components/CheckVerification";
-import Verify from "./components/Verify";
 import Hackathon from "./components/Hackathon";
 
 // Styles
 import "./styles/App.css";
-import SendSMS from "./components/SendSMS";
 
 function App() {
-  const [demoSelection, setDemoSelection] = useState("Select Demo");
-
-  const showOrHideElement = () => {
-    switch (demoSelection) {
-      case "Toll Free Verification":
-        break;
-      case "Account Security":
-        return { display: "none" };
-      case "Voice Intelligence":
-        break;
-      default:
-        return { display: "none" };
-    }
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand>
-              <Dropdown onSelect={(e) => setDemoSelection(e)}>
-                <Dropdown.Toggle> {demoSelection}</Dropdown.Toggle>
-                <Dropdown.Menu className="dropdown-menu-dark">
-                  <Dropdown.Item as={Link} to={"/"} href="Select Demo">
-                    Select Demo
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    as={Link}
-                    to={"/verification-form"}
-                    href="Toll Free Verification"
-                  >
-                    Toll Free Verification
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    as={Link}
-                    to={"/verify"}
-                    href="Account Security"
-                  >
-                    Account Security
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    as={Link}
-                    to={"/voice-intelligence"}
-                    href="Voice Intelligence"
-                  >
-                    Voice Intelligence
-                  </Dropdown.Item>
-                  <Dropdown.Item
-                    as={Link}
-                    to={"/hackathon"}
-                    href="Open AI Hackathon"
-                  >
-                    Open AI Hackathon
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Navbar.Brand>
-            <Nav className="justify-content-end">
-              <Nav>
-                <Link
-                  to={"/send-sms"}
-                  className="nav-link"
-                  style={showOrHideElement()}
-                >
-                  Send SMS
-                </Link>
-              </Nav>
-              <Nav>
-                <Link
-                  to={"/check-verification"}
-                  className="nav-link"
-                  style={showOrHideElement()}
-                >
-                  Check Verification Status
-                </Link>
-              </Nav>
-              <Nav>
-                <Link
-                  to={"/event-stream"}
-                  className="nav-link"
-                  style={showOrHideElement()}
-                >
-                  Event Stream
-                </Link>
-              </Nav>
-            </Nav>
+            <Navbar.Brand>Open AI IVR Hackathon Demo</Navbar.Brand>
           </Container>
         </Navbar>
       </header>
@@ -115,17 +28,6 @@ function App() {
             <div className="wrapper">
               <Routes>
                 <Route exact path="/" element={<Home />} />
-                <Route path="/send-sms" element={<SendSMS />}></Route>
-                <Route
-                  path="/verification-form"
-                  element={<VerificationForm />}
-                ></Route>
-                <Route
-                  path="/check-verification"
-                  element={<CheckVerification />}
-                ></Route>
-                <Route path="/event-stream" element={<EventStream />}></Route>
-                <Route path="/verify" element={<Verify />}></Route>
                 <Route path="/hackathon" element={<Hackathon />}></Route>
               </Routes>
             </div>
--- client/src/components/Hackathon.jsx
+++ client/src/components/Hackathon.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import axios from "axios";
+import "../styles/Hackathon.css";
 
 const Hackathon = () => {
-  const [systemContent, setSystemContent] = useState("");
-  const [assistantContent, setAssistantContent] = useState(""); // context
-  const [languageSettings, setLanguageSettings] = useState("");
-  const [functionGenerator, setFunctionGenerator] = useState("");
+  const [systemContext, setSystemContext] = useState(
+    "You are an outbound sales representative selling Apple Airpods. You have a youthful and cheery personality. Keep your responses as brief as possible but make every attempt to keep the caller on the phone without being rude. Don't ask more than 1 question at a time. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. Speak out all prices to include the currency. Please help them decide between the airpods, airpods pro and airpods max by asking questions like 'Do you prefer headphones that go in your ear or over the ear?'. If they are trying to choose between the airpods and airpods pro try asking them if they need noise canceling. Once you know which model they would like ask them how many they would like to purchase and try to get them to place an order. You must add a '•' symbol every 5 to 10 words at natural pauses where your response can be split for text to speech."
+  );
+  const [initialGreeting, setInitialGreeting] = useState(
+    "Hello! I understand you're looking for a pair of AirPods, is that correct?"
+  );
+  const [languageSettings, setLanguageSettings] = useState("English & French");
+  const [functionGenerator, setFunctionGenerator] = useState(
+    "Need to generate function manifest file - Name, description, type (object), parameters, returns "
+  );
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState({
     data: "Hello! I understand you're looking for a pair of AirPods, is that correct?",
   });
 
   const getOpenAIResponse = () => {
-    // alert("Button pushed - update");
     axios
-      .get("http://localhost:3001/hackathon/ask-gpt/" + prompt)
+      .get("http://localhost:3000/hackathon/ask-gpt/" + prompt)
       .then((response) => {
         console.log(response);
         setResponse(response);
@@ -22,34 +28,62 @@ const Hackathon = () => {
       .catch((e) => console.log(e));
   };
 
+  // const createVirtualAgent = () => {
+  //   axios
+  //     .post(
+  //       "http://localhost:3000/hackathon/set-user-context/" +
+  //         initialGreeting +
+  //         "/" +
+  //         systemContext
+  //     )
+  //     .then((response) => {
+  //       console.log(response);
+  //       setResponse(response);
+  //       alert("Success! Created Virtual Agent");
+  //     })
+  //     .catch((e) => {
+  //       alert(e);
+  //       console.log(e);
+  //     });
+  // };
+
   const createVirtualAgent = () => {
-    alert("TBD");
-    // axios
-    //   .get("http://localhost:3001/hackathon/ask-gpt/" + prompt)
-    //   .then((response) => {
-    //     console.log(response);
-    //     setResponse(response);
-    //   })
-    //   .catch((e) => console.log(e));
+    axios
+      .post("http://localhost:3000/hackathon/set-user-context", {
+        greeting: initialGreeting,
+        context: systemContext,
+      })
+      .then((response) => {
+        console.log(response);
+        setResponse(response);
+        alert("Success! Created Virtual Agent");
+      })
+      .catch((e) => {
+        alert(e);
+        console.log(e);
+      });
   };
 
   return (
     <div>
-      <h1>Open AI IVR Hackathon Demo</h1>
       <h2>Bot Builder</h2>
-
+      <p>
+        To do: Function Generator - Need to generate functions & function
+        manifest file Language Generator - Need to define language settings and
+        how this would work with TTS
+      </p>
       <div className="form-group">
-        <label htmlFor="systemContent">System Content</label>
-        <input
-          id="systemContent"
-          className="form-control"
-          value={systemContent}
-          placeholder="System content"
-          onChange={(e) => setSystemContent(e.target.value)}
+        <label htmlFor="systemContext">System Context</label>
+        <textarea
+          id="systemContext"
+          className="form-control w-100 largeInput"
+          value={systemContext}
+          placeholder="System context"
+          onChange={(e) => setSystemContext(e.target.value)}
         />
       </div>
       <div className="form-group">
-        <label htmlFor="assistantContent">Language Settings</label>
+        <label htmlFor="languageSettings">Language Settings</label>
         <input
           id="languageSettings"
           className="form-control"
@@ -59,18 +93,18 @@ const Hackathon = () => {
         />
       </div>
       <div className="form-group">
-        <label htmlFor="assistantContent">Assistant Content</label>
+        <label htmlFor="initialGreeting">Initial Greeting</label>
         <input
-          id="assistantContent"
+          id="initialGreeting"
           className="form-control"
-          value={assistantContent}
-          placeholder="Assistant content"
-          onChange={(e) => setAssistantContent(e.target.value)}
+          value={initialGreeting}
+          placeholder="Initial greeting"
+          onChange={(e) => setInitialGreeting(e.target.value)}
         />
       </div>
       <div className="form-group">
-        <label htmlFor="assistantContent">Function Generator</label>
-        <input
+        <label htmlFor="functionGenerator">Function Generator</label>
+        <textarea
           id="functionGenerator"
           className="form-control"
           value={functionGenerator}
@@ -78,12 +112,14 @@ const Hackathon = () => {
           onChange={(e) => setFunctionGenerator(e.target.value)}
         />
       </div>
-
       <button onClick={createVirtualAgent} className="btn btn-primary">
         Create Virtual Agent
       </button>
       <h2>Bot Tester</h2>
-
+      <p>
+        To Do: Add voice sdk soft client to make call from browser, show output
+        from media in dialogbox as it's spoken
+      </p>
       <div className="form-group">
         <label htmlFor="response">GPT</label>
         <input
@@ -106,7 +142,6 @@ const Hackathon = () => {
       <button onClick={getOpenAIResponse} className="btn btn-primary">
         Ask GPT
       </button>
-      <h2>Number Provisioning</h2>
     </div>
   );
 };
