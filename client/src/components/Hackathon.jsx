1b464cabe7952501c1eed3791530ae3e25b870bb
Kyle Chan <kylechan1112@gmail.com>
2024-01-24 11:06:01 -0800

    First Draft of UI (properties and return object is not updating)

--- client/src/components/Hackathon.jsx
+++ client/src/components/Hackathon.jsx
@@ -2,23 +2,108 @@ import { useState } from "react";
 import axios from "axios";
 import "../styles/Hackathon.css";
 
+
+
 const Hackathon = () => {
+
+  function updateSystemContextField(){
+    // find the dropdown
+    var ddl = document.getElementById("systemContextTemplate");
+    // find the selected option
+    var selectedOption = ddl.options[ddl.selectedIndex];
+    // find the attribute value
+    var mailValue = selectedOption.getAttribute("data-system-context-template");
+    // find the textbox
+    var textBox = document.getElementById("systemContext");
+  
+    // set the textbox value
+    if(mailValue=="OSR"){
+        textBox.value = "ABCD";
+    }
+    else if(mailValue=="ICSR"){
+        textBox.value = "EFGH";
+    } 
+  }
+
   const [systemContext, setSystemContext] = useState(
     "You are an outbound sales representative selling Apple Airpods. You have a youthful and cheery personality. Keep your responses as brief as possible but make every attempt to keep the caller on the phone without being rude. Don't ask more than 1 question at a time. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. Speak out all prices to include the currency. Please help them decide between the airpods, airpods pro and airpods max by asking questions like 'Do you prefer headphones that go in your ear or over the ear?'. If they are trying to choose between the airpods and airpods pro try asking them if they need noise canceling. Once you know which model they would like ask them how many they would like to purchase and try to get them to place an order. You must add a '•' symbol every 5 to 10 words at natural pauses where your response can be split for text to speech."
   );
   const [initialGreeting, setInitialGreeting] = useState(
     "Hello! I understand you're looking for a pair of AirPods, is that correct?"
   );
-  const [languageSettings, setLanguageSettings] = useState("English & French");
-  const [functionGenerator, setFunctionGenerator] = useState(
-    "Need to generate function manifest file - Name, description, type (object), parameters, returns "
-  );
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState({
     data: "Hello! I understand you're looking for a pair of AirPods, is that correct?",
   });
 
+  const [func, setFunc] = useState([]);
+  const [funcName, setFuncName] = useState("");
+  const [funcDesc, setFuncDesc] = useState("");
+  const [prop, setProp] = useState([]);
+  const [propName, setPropName] = useState("");
+  const [propType, setPropType] = useState("");
+  const [propDesc, setPropDesc] = useState("");
+  const [retObj, setRetObj] = useState([]);
+  const [retObjName, setRetObjName] = useState("");
+  const [retObjType, setRetObjType] = useState("");
+  const [retObjDesc, setRetObjDesc] = useState("");
+
+
+  const addFunctionInput = ()=>{
+    setFunc([...func, {
+      name: "",
+      description: "",
+      properties: [],
+      returnObject: []
+    }])
+
+  }
+
+  const handleFunctionChange = i => e => {
+    let newFunc = [...func];
+    newFunc[i][e.target.name] = e.target.value;
+    setFunc(newFunc);
+
+  }
+
+  const addPropertiesInput = i => e => {
+    let newProp = {
+      name: "",
+      type: "",
+      description: ""
+    }
+    let newFunc = [...func];
+    newFunc[i].properties = [...func[i].properties, newProp];
+    setFunc(newFunc);
+  }
+
+  const handlePropertyChange = (i, p) => e => {
+    let newFunc = [...func];
+    newFunc[i]["properties"][p][e.target.name] = e.target.value;
+    setFunc(newFunc);
+  }
+
+  const addReturnObjectInput = i => e => {
+    let newRetObj = {
+      name: "",
+      type: "",
+      description: ""
+    }
+    let newFunc = [...func];
+    newFunc[i].returnObject = [...func[i].returnObject, newRetObj];
+    setFunc(newFunc);
+
+  }
+
+
+  const handleReturnObjectChange = (i, r) => e => {
+    let newFunc = [...func];
+    newFunc[i]["properties"][r][e.target.name] = e.target.value;
+    setFunc(newFunc);
+  }
+
   const getOpenAIResponse = () => {
+    console.log(func);
     axios
       .get("http://localhost:3000/hackathon/ask-gpt/" + prompt)
       .then((response) => {
@@ -48,6 +133,7 @@ const Hackathon = () => {
   // };
 
   const createVirtualAgent = () => {
+    console.log(func)
     axios
       .post("http://localhost:3000/hackathon/set-user-context", {
         greeting: initialGreeting,
@@ -73,6 +159,15 @@ const Hackathon = () => {
         how this would work with TTS
       </p>
       <div className="form-group">
+        <label htmlFor="systemContextTemplate">System Context Templates</label>
+        <br/>
+        <select defaultValue="blank" id="systemContextTemplate" onChange={updateSystemContextField}>
+          <option data-system-context-template="" value="blank"></option>
+          <option data-system-context-template="OSR" value="Outbound Sales Representative">Outbound Sales Representative</option>
+          <option data-system-context-template="ICSR" value="Inbound Customer Service Representative">Inbound Customer Service Representative</option>
+        </select>
+      </div>
+      <div className="form-group">
         <label htmlFor="systemContext">System Context</label>
         <textarea
           id="systemContext"
@@ -84,13 +179,23 @@ const Hackathon = () => {
       </div>
       <div className="form-group">
         <label htmlFor="languageSettings">Language Settings</label>
-        <input
+        <br/>
+        <input type="checkbox" id="english"/>English
+        <br/>
+        <input type="checkbox" id="french"/>French
+        <br/>
+        <input type="checkbox" id="spanish"/>Spanish
+        <br/>
+        <input type="checkbox" id="italian"/>Italian
+        <br/>
+        <input type="checkbox" id="mandarin"/>Mandarin
+        {/* <input
           id="languageSettings"
           className="form-control"
           value={languageSettings}
           placeholder="Language settings"
           onChange={(e) => setLanguageSettings(e.target.value)}
-        />
+        /> */}
       </div>
       <div className="form-group">
         <label htmlFor="initialGreeting">Initial Greeting</label>
@@ -103,14 +208,115 @@ const Hackathon = () => {
         />
       </div>
       <div className="form-group">
-        <label htmlFor="functionGenerator">Function Generator</label>
-        <textarea
-          id="functionGenerator"
-          className="form-control"
-          value={functionGenerator}
-          placeholder="Function generator"
-          onChange={(e) => setFunctionGenerator(e.target.value)}
-        />
+        <b><label htmlFor="functionList">List of Functions</label></b>
+        <div>
+          <button onClick={()=>addFunctionInput()}>+</button>
+          {func.map((item, i) => {
+            let FunctionName = "function" + i;
+            return (
+              <div className={FunctionName}>
+                <label htmlFor={FunctionName}>&nbsp;&nbsp;&nbsp;Function {i}</label>
+                <br/>
+                <label htmlFor={FunctionName+"Name"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Function Name:</label>
+                <input
+                  onChange={handleFunctionChange(i)} 
+                  value={item.value}
+                  name="name"
+                  id={FunctionName+"Name"}
+                  type={item.type}
+                  size="40"
+                />
+                <br/>
+                <label htmlFor={FunctionName+"Desc"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Function Description:</label>
+                <input
+                  onChange={handleFunctionChange(i)} 
+                  value={item.value}
+                  name="description"
+                  id={FunctionName+"Desc"}
+                  type={item.type}
+                  size="40"
+                />
+                <br/>
+                <b><label htmlFor={FunctionName+"PropertyList"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Properties</label></b>
+                <button onClick={addPropertiesInput(i)}>+</button>
+                  {func.map((item, p) => {
+                    let PropName = FunctionName + "Property" + p;
+                    return (
+                      <div className={PropName}>
+                        <label htmlFor={PropName}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Property {p}</label>
+                        <br/>
+                        <label htmlFor={PropName+"Name"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Property Name:</label>
+                        <input
+                        onChange={handlePropertyChange(i, p)}
+                          value={item.value}
+                          name="name"
+                          id={PropName+"Name"}
+                          type={item.type}
+                          size="40"
+                        />
+                        <br/>
+                        <label htmlFor={PropName+"Type"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Property Type:</label>
+                        <select defaultValue="blank" id={PropName+"Type"} name="type" onChange={handlePropertyChange(i, p)}>
+                          <option data-system-context-template="" value="blank"></option>
+                          <option data-system-context-template="String" value="String">String</option>
+                          <option data-system-context-template="Integer" value="Integer">Integer</option>
+                        </select>
+                        <br/>
+                        <label htmlFor={PropName+"Desc"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Property Description:</label>
+                        <input
+                        onChange={handlePropertyChange(i, p)} 
+                          value={item.value}
+                          name="description"
+                          id={PropName+"Name"}
+                          type={item.type}
+                          size="40"
+                        />
+                      </div>
+                    )
+                  })}
+                <br/>
+                <b><label htmlFor={FunctionName+"ReturnObj"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;List of Return Objects</label></b>
+                <button onClick={addReturnObjectInput(i)}>+</button>
+                {func.map((item, r) => {
+                    let RetObjName = FunctionName + "RetObj" + r;
+                    return (
+                      <div className={RetObjName}>
+                        <label htmlFor={RetObjName}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return Object {r}</label>
+                        <br/>
+                        <label htmlFor={RetObjName+"Name"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return Object Name:</label>
+                        <input
+                        onChange={handleReturnObjectChange(i, r)} 
+                          value={item.value}
+                          name="name"
+                          id={RetObjName+"Nam"}
+                          type={item.type}
+                          size="40"
+                        />
+                        <br/>
+                        <label htmlFor={RetObjName+"Type"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return Object Type:</label>
+                        <select defaultValue="blank" id={RetObjName+"Type"} name="type" onChange={handleReturnObjectChange(i, r)}>
+                          <option data-system-context-template="" value="blank"></option>
+                          <option data-system-context-template="String" value="String">String</option>
+                          <option data-system-context-template="Integer" value="Integer">Integer</option>
+                        </select>
+                        <br/>
+                        <label htmlFor={RetObjName+"Desc"}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Return Object Description:</label>
+                        <input
+                        onChange={handleReturnObjectChange(i, r)} 
+                          value={item.value}
+                          name="description"
+                          id={RetObjName+"Name"}
+                          type={item.type}
+                          size="40"
+                        />
+                      </div>
+                    )
+                  })}
+                <br/>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <button onClick={createVirtualAgent} className="btn btn-primary">
         Create Virtual Agent
